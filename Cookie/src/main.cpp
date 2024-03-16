a1c1c6a440f059c6f6c5d8d6311adc3c728ed033
NateEbling <ebling.nathan@gmail.com>
2024-03-15 20:55:18 -0400

    Template ready to be used

---
+++ Cookie/src/main.cpp
@@ -0,0 +1,12 @@
+#include "core.h"
+
+
+int main()
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	return 0;
+}
