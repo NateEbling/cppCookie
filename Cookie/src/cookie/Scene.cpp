9e9b0dd584a0a53e6346b13872734701a9f68cad
Nate <ebling.nathan@gmail.com>
2024-03-21 19:05:04 -0400

    Added dev log and several .cpp and .h files

--- Cookie/src/cookie/Scene.cpp
+++ Cookie/src/cookie/Scene.cpp
@@ -1,5 +1,6 @@
 #include "core.h"
 #include "cookie/Scene.h"
+#include "renderer/Shader.h"
 
 namespace Cookie
 {
@@ -8,15 +9,19 @@ namespace Cookie
 		static SceneType currentScene = SceneType::None;
 		static SceneType nextSceneType = SceneType::None;
 		static bool changeSceneAtFrameEnd = false;
+		Shader* defaultShader;
 
 		void init(SceneType type)
 		{
-
+			defaultShader = new Shader();
+			defaultShader->compile("assets/shaders/default.glsl");
 		}
 
 		void update(float dt)
 		{
-			
+			defaultShader->bind();
+
+			defaultShader->unbind();
 		}
 	}
 }
